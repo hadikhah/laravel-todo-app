290c5fd5ad41ccdd64053b4b43c1eca0eccdeb03
hadikhah <ma.hadikhah@raahin.com>
2022-01-18 20:15:00 +0330

    bind additional input props to Input.vue component

--- resources/js/Components/Input.vue
+++ resources/js/Components/Input.vue
@@ -1,7 +1,7 @@
 <template>
   <input
       class="mt-1 border-gray-300 rounded-md shadow-sm  focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus-within:text-primary-600"
-      :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="input"/>
+      :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="input" v-bind="$props" />
 </template>
 
 <script>
